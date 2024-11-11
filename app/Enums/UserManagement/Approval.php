62c02e9f0ceb56b8437b67822b95aa838b2bbd16
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-11 22:57:45 +0600

    approval enum created

---
+++ app/Enums/UserManagement/Approval.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enums\UserManagement;
+
+use Spatie\Enum\Laravel\Enum;
+
+final class Approval extends Enum {
+    const PENDING = "pending" ;
+    const APPROVED = "approved" ;
+    const SUSPENDED = "suspended" ;
+
+    /**
+     * Get all status values
+     *
+     * @return array
+     */
+    public static function fetch() {
+        return [
+            self::PENDING,
+            self::APPROVED,
+            self::SUSPENDED,
+        ];
+    }
+}
